6b7bc09926723e902dcfd305e16a16c64aaefffc
katamartin <katasm@gmail.com>
2023-05-09 09:42:16 -0700

    Add basic sorting

--- components/projects.js
+++ components/projects.js
@@ -1,9 +1,12 @@
 import { Column, Row } from '@carbonplan/components'
+import { Triangle } from '@carbonplan/icons'
 import useSWR from 'swr'
+import { Box } from 'theme-ui'
+import { useCallback, useState } from 'react'
 
 const fetcher = (url) => fetch(url).then((r) => r.json())
 
-const TableRow = ({ values, as, sx }) => {
+const TableRow = ({ values, as, sx, Button }) => {
   const starts = values.reduce((accum, v, i) => {
     if (accum.length === 0) {
       accum.push(1)
@@ -24,6 +27,7 @@ const TableRow = ({ values, as, sx }) => {
           width={value?.width ?? 1}
           sx={{ fontSize: 1, textAlign: 'left', ...sx }}
         >
+          {Button && <Button value={value} />}
           {value?.label ?? value}
         </Column>
       ))}
@@ -31,15 +35,54 @@ const TableRow = ({ values, as, sx }) => {
   )
 }
 
-const TableHead = ({ values }) => {
+const TableHead = ({ values, sort, setSort }) => {
+  const Sort = useCallback(
+    ({ value }) => {
+      return (
+        <Box
+          as='button'
+          sx={{
+            width: '16px',
+            height: '16px',
+            bg: 'transparent',
+            border: 'none',
+            p: 0,
+            m: 0,
+            display: 'block',
+            cursor: 'pointer',
+            '@media (hover: hover) and (pointer: fine)': {
+              [`&:hover #${value.value}-triangle`]: {
+                stroke: 'primary',
+              },
+            },
+          }}
+          onClick={() => setSort(value.value)}
+        >
+          <Triangle
+            id={`${value.value}-triangle`}
+            sx={{
+              transition: 'stroke 0.15s',
+              stroke: sort === value.value ? 'primary' : 'muted',
+              fill: 'none',
+              width: 10,
+              height: 10,
+            }}
+          />
+        </Box>
+      )
+    },
+    [sort]
+  )
+
   return (
     <thead>
-      <TableRow as='th' values={values} />
+      <TableRow as='th' values={values} Button={Sort} />
     </thead>
   )
 }
 
 const Projects = () => {
+  const [sort, setSort] = useState('project_id')
   const { data, error, isLoading } = useSWR(
     'https://offsets-db.fly.dev/projects',
     fetcher
@@ -50,30 +93,34 @@ const Projects = () => {
   return (
     <table>
       <TableHead
+        sort={sort}
+        setSort={setSort}
         values={[
-          'Project ID',
-          { label: 'Name', width: 4 },
-          'Country',
-          'Registered',
+          { value: 'project_id', label: 'Project ID' },
+          { value: 'name', label: 'Name', width: 4 },
+          { value: 'country', label: 'Country' },
+          { value: 'registered_at', label: 'Registered' },
         ]}
       />
       <tbody>
-        {data.map((d) => (
-          <TableRow
-            key={d.project_id}
-            values={[
-              d.project_id,
-              { label: d.name, width: 4 },
-              d.country,
-              d.registered_at,
-            ]}
-            sx={{
-              color: 'secondary',
-              fontFamily: 'mono',
-              letterSpacing: 'mono',
-            }}
-          />
-        ))}
+        {data
+          .sort((a, b) => a[sort]?.localeCompare(b[sort]))
+          .map((d) => (
+            <TableRow
+              key={d.project_id}
+              values={[
+                d.project_id,
+                { label: d.name, width: 4 },
+                d.country,
+                d.registered_at,
+              ]}
+              sx={{
+                color: 'secondary',
+                fontFamily: 'mono',
+                letterSpacing: 'mono',
+              }}
+            />
+          ))}
       </tbody>
     </table>
   )
