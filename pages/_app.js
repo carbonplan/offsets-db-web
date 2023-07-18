703fe489a3698fc6272e4a74c827286c4d876ded
katamartin <katasm@gmail.com>
2023-07-18 15:34:22 -0700

    Create dedicated routes for projects and credits lists

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,30 @@ import { MDXProvider } from '@mdx-js/react'
 import '@carbonplan/components/fonts.css'
 import '@carbonplan/components/globals.css'
 import theme from '@carbonplan/theme'
+import { Layout } from '@carbonplan/components'
+import { Container } from 'theme-ui'
+import { QueryProvider } from '../components/queries'
 
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
       <MDXProvider>
-        <Component {...pageProps} />
+        <Layout
+          title='Offsets DB – CarbonPlan'
+          description='TK'
+          card='TK'
+          footer={false}
+          metadata={false}
+          container={false}
+          nav={'research'}
+          url={'https://carbonplan.org/research/offsets-database'}
+        >
+          <Container>
+            <QueryProvider>
+              <Component {...pageProps} />
+            </QueryProvider>
+          </Container>
+        </Layout>
       </MDXProvider>
     </ThemeProvider>
   )
---
+++ pages/credits.js
@@ -0,0 +1,6 @@
+import Index from '../components/index.js'
+
+const CreditsIndex = () => {
+  return <Index mode='credits' />
+}
+export default CreditsIndex
