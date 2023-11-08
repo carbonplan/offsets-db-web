28af19d0b29038829129176e1537430629b6f8f7
katamartin <katasm@gmail.com>
2023-11-08 16:58:53 -0500

    Add basic maple bar charts

---
+++ components/charts/category-bar.js
@@ -0,0 +1,37 @@
+import { Badge } from '@carbonplan/components'
+import { format } from 'd3-format'
+import { Box, Flex } from 'theme-ui'
+
+import { COLORS, LABELS } from '../constants'
+
+const CategoryBar = ({ label, total, mapping }) => {
+  return (
+    <Box sx={{ mt: 3 }}>
+      <Flex sx={{ gap: 3, alignItems: 'flex-end' }}>
+        <Box
+          sx={{ fontFamily: 'mono', letterSpacing: 'mono', color: 'secondary' }}
+        >
+          {label}
+        </Box>
+        <Badge sx={{ fontSize: 4, height: ['34px'], px: 1, mb: '-2px' }}>
+          {total > 100 ? format('.3s')(total) : total}
+        </Badge>
+      </Flex>
+
+      <Flex sx={{ height: '20px', mt: 3 }}>
+        {Object.keys(LABELS.category).map((l) => (
+          <Box
+            key={l}
+            sx={{
+              height: '20px',
+              width: `${(mapping[l] / total) * 100}%`,
+              backgroundColor: COLORS[l],
+            }}
+          />
+        ))}
+      </Flex>
+    </Box>
+  )
+}
+
+export default CategoryBar
--- components/charts/project-charts.js
+++ components/charts/project-charts.js
@@ -1,9 +1,52 @@
 import { Column, Row } from '@carbonplan/components'
-import ProjectCredits from './project-credits'
+import { useMemo } from 'react'
 
-import ProjectRegistration from './project-registration'
+import { LABELS } from '../constants'
+import useFetcher from '../use-fetcher'
+import CategoryBar from './category-bar'
 
 const ProjectCharts = () => {
+  const { data, error, isLoading } = useFetcher(
+    'charts/credits_by_category',
+    {}
+  )
+
+  const { issued, retired } = useMemo(() => {
+    const empty = {
+      issued: { total: 0, mapping: {} },
+      retired: { total: 0, mapping: {} },
+    }
+    if (!data) {
+      return empty
+    }
+
+    return data.data.reduce(
+      (
+        { issued: prevIssued, retired: prevRetired },
+        { category, retired, issued }
+      ) => {
+        const key = LABELS.category[category] ? category : 'other'
+        prevIssued.mapping[key] = prevIssued.mapping[key]
+          ? prevIssued.mapping[key] + issued
+          : issued
+        prevRetired.mapping[key] = prevRetired.mapping[key]
+          ? prevRetired.mapping[key] + retired
+          : retired
+        return {
+          issued: {
+            total: prevIssued.total + issued,
+            mapping: prevIssued.mapping,
+          },
+          retired: {
+            total: prevRetired.total + retired,
+            mapping: prevRetired.mapping,
+          },
+        }
+      },
+      empty
+    )
+  }, [data])
+
   return (
     <Row
       columns={[6, 8, 8, 8]}
@@ -15,10 +58,10 @@ const ProjectCharts = () => {
       }}
     >
       <Column start={1} width={[6, 4, 4, 4]}>
-        <ProjectRegistration />
+        <CategoryBar label='Credits issued' {...issued} />
       </Column>
       <Column start={[1, 5, 5, 5]} width={[6, 4, 4, 4]}>
-        <ProjectCredits />
+        <CategoryBar label='Credits retired' {...retired} />
       </Column>
     </Row>
   )
