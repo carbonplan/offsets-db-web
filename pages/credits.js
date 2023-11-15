c0c4ed2477e8c64bb4b4cbe448928f9a5fdefdd6
katamartin <katasm@gmail.com>
2023-11-15 15:20:27 -0500

    Clean up credits page

--- pages/credits.js
+++
@@ -1,6 +0,0 @@
-import Index from '../components/index.js'
-
-const CreditsIndex = () => {
-  return <Index mode='credits' />
-}
-export default CreditsIndex
--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,37 @@
-import Index from '../components/index.js'
+import { Layout as PageLayout } from '@carbonplan/components'
+import { Container } from 'theme-ui'
 
-const ProjectsIndex = () => {
-  return <Index mode='projects' />
+import Layout from '../components/layout'
+import Projects from '../components/projects'
+import Queries from '../components/queries'
+import Sidebar from '../components/sidebar'
+
+const Index = () => {
+  return (
+    <PageLayout
+      title='Offsets DB – CarbonPlan'
+      description='TK'
+      card='TK'
+      dimmer='top'
+      footer={false}
+      metadata={false}
+      container={false}
+      nav={'research'}
+      url={'https://carbonplan.org/research/offsets-db'}
+    >
+      <Container>
+        <Layout
+          sidebar={
+            <Sidebar>
+              <Queries />
+            </Sidebar>
+          }
+        >
+          <Projects />
+        </Layout>
+      </Container>
+    </PageLayout>
+  )
 }
-export default ProjectsIndex
+
+export default Index
