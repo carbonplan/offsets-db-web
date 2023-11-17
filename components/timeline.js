a4ec6a49ea99c7867397449bed0fab3c8061088e
katamartin <katasm@gmail.com>
2023-11-17 11:33:22 -0500

    Tweak date entries in Timeline

--- components/timeline.js
+++ components/timeline.js
@@ -14,8 +14,9 @@ const Timeline = ({ project, color }) => {
           label: title,
           url,
         })),
-        { date: project.listed_at, label: 'Project registered' },
-        { date: project.started_at, label: 'Project listed' },
+        { date: project.listed_at, label: 'Project listed' },
+        { date: project.first_issuance_at, label: 'First credits issued' },
+        { date: project.first_retirement_at, label: 'First credits retired' },
       ]
         .filter((d) => d.date)
         .sort((a, b) => b.date.localeCompare(a.date)),
