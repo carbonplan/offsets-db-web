a83bddad2013cefafaf4d058ca6d60968759b8b0
katamartin <katasm@gmail.com>
2023-12-07 10:04:46 -0500

    Shrink mb on OffsetsDB header; no longer baseline-aligned with maple bar

--- components/sidebar.js
+++ components/sidebar.js
@@ -35,7 +35,7 @@ const Sidebar = ({ children }) => {
           pb: [4, 3, 5, 5],
         }}
       >
-        <Box as='h1' sx={{ variant: 'styles.h1', mt: 4 }}>
+        <Box as='h1' sx={{ variant: 'styles.h1', mt: 4, mb: 3 }}>
           OffsetsDB
         </Box>
         <Box as='p' sx={{ mb: 5 }}>
