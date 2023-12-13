b916e59a1c1a7fab7ec6b1ec0c5f6ec8cbd7e24f
katamartin <katasm@gmail.com>
2023-12-13 11:54:54 -0500

    Fix pass-through of parameters

--- pages/api/query.js
+++ pages/api/query.js
@@ -1,8 +1,22 @@
+function constructSearch(search = {}) {
+  const params = new URLSearchParams()
+
+  Object.keys(search).forEach((key) => {
+    const value = search[key]
+    if (Array.isArray(value)) {
+      value.forEach((entry) => params.append(key, entry))
+    } else {
+      params.append(key, value)
+    }
+  })
+
+  return params.toString()
+}
 export default async function handler(req, res) {
   try {
     const { path, ...search } = req.query
     const reqUrl = new URL(`${process.env.NEXT_PUBLIC_API_URL}/${path}`)
-    reqUrl.search = search.toString()
+    reqUrl.search = constructSearch(search)
     const serverRes = await fetch(reqUrl)
     const result = await serverRes.json()
     res.status(200).send(result)
