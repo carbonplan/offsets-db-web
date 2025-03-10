f775339812f59f4a189f70b9ac935d812002a5ca
Kata Martin <katasm@gmail.com>
2025-03-10 10:44:53 -0400

    Set up shared layout for landing pages

---
+++ components/landing-layout.js
@@ -0,0 +1,44 @@
+import { Column, Layout, Row } from '@carbonplan/components'
+import { Container, Divider } from 'theme-ui'
+import Sidebar from './sidebar'
+import Queries from './queries'
+import ProjectCharts from './charts/project-charts'
+
+const LandingLayout = ({ children }) => {
+  return (
+    <Layout
+      title='OffsetsDB – CarbonPlan'
+      description='A harmonized database of carbon offset projects and credits.'
+      card='https://images.carbonplan.org/social/offsets-db.png'
+      dimmer='top'
+      footer={false}
+      metadata={false}
+      container={false}
+      nav={'research'}
+      url={'https://carbonplan.org/research/offsets-db'}
+    >
+      <Container>
+        <Row>
+          <Sidebar>
+            <Queries />
+          </Sidebar>
+          <Column start={[1, 1, 5, 5]} width={[6, 8, 8, 8]}>
+            <Divider
+              sx={{
+                display: ['inherit', 'inherit', 'none', 'none'],
+                ml: [-4, -5, -5, -6],
+                mr: [-4, -5, 0, 0],
+                mt: 3,
+                mb: 2,
+              }}
+            />
+            <ProjectCharts />
+            {children}
+          </Column>
+        </Row>
+      </Container>
+    </Layout>
+  )
+}
+
+export default LandingLayout
--- pages/credits.js
+++ pages/credits.js
@@ -1,39 +1,12 @@
-import { Layout as PageLayout } from '@carbonplan/components'
-import { Container } from 'theme-ui'
-
-import Layout from '../components/layout'
-import Queries from '../components/queries'
-import Sidebar from '../components/sidebar'
 import Credits from '../components/credits'
-import ProjectCharts from '../components/charts/project-charts'
+import LandingLayout from '../components/landing-layout'
 
-const Index = () => {
-  return (
-    <PageLayout
-      title='OffsetsDB – CarbonPlan'
-      description='A harmonized database of carbon offset projects and credits.'
-      card='https://images.carbonplan.org/social/offsets-db.png'
-      dimmer='top'
-      footer={false}
-      metadata={false}
-      container={false}
-      nav={'research'}
-      url={'https://carbonplan.org/research/offsets-db'}
-    >
-      <Container>
-        <Layout
-          sidebar={
-            <Sidebar>
-              <Queries />
-            </Sidebar>
-          }
-        >
-          <ProjectCharts />
-          <Credits />
-        </Layout>
-      </Container>
-    </PageLayout>
-  )
+const Page = () => {
+  return <Credits />
 }
 
-export default Index
+Page.getLayout = function getLayout(page) {
+  return <LandingLayout>{page}</LandingLayout>
+}
+
+export default Page
