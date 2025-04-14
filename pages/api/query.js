29a74d1ae7ecf28aca98c27c212fabe5aed562c1
Kata Martin <katasm@gmail.com>
2025-04-14 12:00:50 -0400

    Try adding middleware to handle CORS

--- pages/api/query.js
+++ pages/api/query.js
@@ -13,8 +13,6 @@ function constructSearch(search = {}) {
   return params.toString()
 }
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*.carbonplan.org')
-
   try {
     const { path, ...search } = req.query
     const reqUrl = new URL(`${process.env.NEXT_PUBLIC_API_URL}/${path}`)
